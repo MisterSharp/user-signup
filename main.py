1a9051f23c891ca77d7d8f15d2c7c3dccd0da89b
Jarrod Sharp <jarrodksharp@gmail.com>
2019-05-14 18:56:29 -0500

    Added sign-up template & first app route to main.py

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-import
+from flask import Flask, request, redirect, render_template
+import cgi
+import os
+
+
+app = Flask(__name__)
+
+app.config['DEBUG'] = True
+
+
+@app.route("/")
+def index():
+    return render_template('sign-up.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.run()
