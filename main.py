f018b3eaeb47fae81619f178d12258695f5f7901
Jarrod Sharp <jarrodksharp@gmail.com>
2019-05-15 22:40:24 -0500

    Updated logic for all form fields & created welcome template..working

--- main.py
+++ main.py
@@ -19,14 +19,10 @@ def index():
     email_error = ""
     username = ""
     email = ""
+    password = ""
+    verify = ""
 
-
-
-    
-    
-    
     if request.method == 'POST': #validing the form fields
-
         username = request.form['Username']
         email = request.form['Email']
         #is Username blank?
@@ -35,10 +31,15 @@ def index():
         #is it between 3 & 20 characters?
         if len(request.form['Username']) < 3 or len(request.form['Username']) > 20:
             username_error += "Username must be between 3-20 characters"
+        #is password blank?
+        if " " in request.form['Password']:
+            password_error += "Spaces are not allowed in the password"
+        #is it between 3 & 20 characters?
+        if len(request.form['Password']) < 3 or len(request.form['Password']) > 20:
+            password_error += "Password must be between 3-20 characters"
         #do passwords match?
-        #if request.form['']:
-            #pass
-        #if request.form['']
+        if request.form['Verify'] != request.form['Password']:
+            verify_error += "Passwords do not match"
         
         
         #if the Email field is not blank
@@ -47,7 +48,12 @@ def index():
             if not validators.email(request.form['Email']):
                 email_error = 'Enter a valid e-mail or leave blank'
 
-    return render_template('sign-up.html', username_error=username_error, password_error=password_error, verify_error=verify_error, email_error=email_error, username=username, email=email)
+    return render_template('sign-up.html', username_error=username_error, password_error=password_error, verify_error=verify_error, email_error=email_error, username=username, email=email, password=password, verify=verify)
+
+@app.route("/welcome")
+def valid():
+    user = request.form['Username']
+    return render_template('welcome.html', user=user)
 
 
 
